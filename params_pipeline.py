32dc50ecb6fd5d123fc6206ad1a419939aff13c3
mh-vasconcelos <66500702+mh-vasconcelos@users.noreply.github.com>
2025-06-21 01:24:27 -0300

    Update params_pipeline.py

--- params_pipeline.py
+++ params_pipeline.py
@@ -1,13 +1,2 @@
 url = 'https://www.sofascore.com/pt/time/futebol/vasco-da-gama/1974'
 ano_limite = '/24'
-
-data = {
-    "data": [],
-    "time_casa": [],
-    "time_fora": [],
-    "placar_casa": [],
-    "placar_fora": [],
-    "campeonato": [],
-    "status": [],
-    "resultado": []
-}
